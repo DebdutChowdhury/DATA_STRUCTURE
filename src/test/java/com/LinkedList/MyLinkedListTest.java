27185db6afc2ccb75f49bf424eb93389a48437cc
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-10 17:43:26 +0530

    Added: Create LinkedList using append

--- src/test/java/com/LinkedList/MyLinkedListTest.java
+++ src/test/java/com/LinkedList/MyLinkedListTest.java
@@ -19,4 +19,21 @@ public class MyLinkedListTest {
         boolean result = myLinkedList.head.equals(mythrdNode) && myLinkedList.head.getNext().equals(mysndNode) && myLinkedList.tail.equals(myfstNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3number_WhenAppend_ShouldbeAddedToLast() {
+        MyNode<Integer> mysndNode = new MyNode<>(30);
+        MyNode<Integer> mythrdNode = new MyNode<>(70);
+        MyNode<Integer> myfstNode = new MyNode<>(56);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myfstNode);
+        myLinkedList.append(mysndNode);
+        myLinkedList.append(mythrdNode);
+
+        boolean result = myLinkedList.head.equals(myfstNode) && myLinkedList.head.getNext().equals(mysndNode) &&
+                            myLinkedList.tail.equals(mythrdNode);
+        Assert.assertTrue(result);
+
+    }
 }
