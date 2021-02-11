7119cea07cb21f2636c20fda5eb401665bd69b24
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-11 21:53:00 +0530

    Added: create a new stack here

---
+++ src/main/java/com/LinkedList/MyStack.java
@@ -0,0 +1,22 @@
+package com.LinkedList;
+
+public class MyStack {
+
+    private final MyLinkedList myLinkedList;
+
+    public MyStack() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public void push(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+    public void printMyStack() {
+        myLinkedList.printMyNode();
+    }
+}
---
+++ src/test/java/com/LinkedList/MyStackTest.java
@@ -0,0 +1,21 @@
+package com.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyStackTest {
+
+    @Test
+    public void given3number_whenadded_stackSouldhaveLastAddNode() {
+        MyStack mystack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        mystack.push(myFirstNode);
+        mystack.push(mySecondNode);
+        mystack.push(myThirdNode);
+        mystack.printMyStack();
+        INode myNode = mystack.peak();
+        Assert.assertEquals(myThirdNode, myNode);
+    }
+}
