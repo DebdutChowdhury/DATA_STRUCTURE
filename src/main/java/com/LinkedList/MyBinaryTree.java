88231d2ef5f6701b3a6274d30de3f6f99b745c76
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-18 21:59:01 +0530

    Added: search method for search by integer in binary tree

--- src/main/java/com/LinkedList/MyBinaryTree.java
+++ src/main/java/com/LinkedList/MyBinaryTree.java
@@ -2,6 +2,7 @@ package com.LinkedList;
 
 public class MyBinaryTree<K extends Comparable<K>> {
     private MyBinaryNode<K> root;
+    private boolean returndResult = false;
 
     public void add(K key) {
         this.root = this.addRecursively(root, key);
@@ -31,4 +32,29 @@ public class MyBinaryTree<K extends Comparable<K>> {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
     }
 
+    public boolean search(K key) {
+        boolean result = this.recursivelySearch(this.root, key);
+        if (returndResult == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean recursivelySearch(MyBinaryNode<K> current, K key) {
+        if (current == null) {
+            return false;
+        }
+
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) {
+            this.returndResult = true;
+            return true;
+        }
+        if (compareResult < 0) {
+            recursivelySearch(current.left, key);
+        } else {
+            recursivelySearch(current.right, key);
+        }
+        return false;
+    }
 }
