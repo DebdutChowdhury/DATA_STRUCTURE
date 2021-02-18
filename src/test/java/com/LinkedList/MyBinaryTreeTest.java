f20e8f2a02d032481980c7297f83e4a896c24734
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-18 19:21:07 +0530

    Added: Ability to create a BST by adding integers

---
+++ src/test/java/com/LinkedList/MyBinaryTreeTest.java
@@ -0,0 +1,17 @@
+package com.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void givenThreeNumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
+}
+
